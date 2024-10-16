21284cf032f70a87e7fc2a2561e81fb21874cf93
Chitong Ung <cung@telitem.com>
2024-10-16 11:55:32 +0200

    ticker

--- main.cpp
+++ main.cpp
@@ -6,19 +6,34 @@
 #include "mbed.h"
 
 // Blinking rate in milliseconds
-#define POLLING_RATE     100ms
+#define POLLING_RATE 100ms
 
 InterruptIn mypin(BUTTON1); // change this to the button on your board
 DigitalOut myled(LED1);
 Timer t;
 bool t_has_value = false;
+Ticker flipper;
+float tick_value = 0.5;
 
 void led_on()
 {
-    t.reset();
-    t.start();
-    t_has_value = false;
+
     myled = !myled;
+
+    // t.reset();
+    // t.start();
+    // t_has_value = false;
+    // myled = !myled;
+}
+void ChangeFreq()
+{
+    tick_value = tick_value + 1.0;
+
+    if (tick_value <= 0.0)
+        tick_value = 0.5;
+
+    flipper.detach();
+    flipper.attach(&led_on, tick_value);
 }
 
 void led_off()
@@ -31,17 +46,18 @@ void led_off()
 int main()
 {
 
-    mypin.rise(&led_on);
+    mypin.rise(&ChangeFreq);
 
-    mypin.fall(&led_off);
+    // mypin.fall(&led_off);
+    flipper.attach(&led_on, tick_value);
 
     // press the button and see the console / led change
-    while (1) {
-        
-        if (t_has_value) {
-            t_has_value = false;
-            printf("The time taken was %llu milliseconds\n", t.elapsed_time().count());
-        }
+    while (1)
+    {
+        /*if (t_has_value) {
+        t_has_value = false;
+        printf("The time taken was %llu milliseconds\n", t.elapsed_time().count());
+        }*/
 
         ThisThread::sleep_for(POLLING_RATE);
     }
