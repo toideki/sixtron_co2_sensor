3f26727a4a0f30e2351e257610859fc54524bbbd
Chitong Ung <cung@telitem.com>
2024-10-16 11:36:19 +0200

    timer value

--- main.cpp
+++ main.cpp
@@ -6,26 +6,43 @@
 #include "mbed.h"
 
 // Blinking rate in milliseconds
-#define POLLING_RATE     250ms
+#define POLLING_RATE     100ms
 
 InterruptIn mypin(BUTTON1); // change this to the button on your board
 DigitalOut myled(LED1);
+Timer t;
+bool t_has_value = false;
 
-void led_on_off()
+void led_on()
 {
+    t.reset();
+    t.start();
+    t_has_value = false;
+    myled = !myled;
+}
+
+void led_off()
+{
+    t.stop();
+    t_has_value = true;
     myled = !myled;
 }
 
 int main()
 {
 
-    mypin.rise(&led_on_off);
+    mypin.rise(&led_on);
 
-    mypin.fall(&led_on_off);
+    mypin.fall(&led_off);
 
     // press the button and see the console / led change
     while (1) {
-        //printf("mypin has value : %d \n\r", mypin.read());
+        
+        if (t_has_value) {
+            t_has_value = false;
+            printf("The time taken was %llu milliseconds\n", t.elapsed_time().count());
+        }
+
         ThisThread::sleep_for(POLLING_RATE);
     }
 }
